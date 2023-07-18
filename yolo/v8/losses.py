beb70dc1f2cdfef1f7ed2b993df5028ffa9153fd
zhangchuqi <1290450579@qq.com>
2023-07-18 09:56:50 +0800

    init boxs iou and nms

---
+++ yolo/v8/losses.py
@@ -0,0 +1,60 @@
+"""
+Object Loss functions
+"""
+import numpy as np
+import torch
+import torchvision
+
+def iou_box():
+    pass
+
+def giou_box():
+    pass
+
+
+def diou_box():
+    pass
+
+def ciou_box():
+    pass
+
+def eiou_box():
+    pass
+
+def focal_eiou_box():
+    pass
+
+def nms():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
